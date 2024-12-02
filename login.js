d9f940c487380b822f5331b6003d5d7030668412
pallab Biswas <pallab.b7@gmail.com>
2024-12-02 21:37:26 +0600

    user button added

--- login.js
+++ login.js
@@ -4,7 +4,20 @@ document.getElementById('button-login').addEventListener('click', function (even
   //step-2 prevent default behavior or page reload
   event.preventDefault();
   console.log('login button clicked');
-  // step-3 get the phone number
+  // step-3 get the phone number & pin number
   const mobileNumber = document.getElementById('mobile-number').value;
-  console.log(mobileNumber);
+  const pinNumber = document.getElementById('pin-number').value;
+
+  // console.log(mobileNumber, pinNumber);
+
+  // step-4 validate phone or pin
+  // this is temporary way to cheque 
+  if (mobileNumber === '5' && pinNumber === '1234') {
+    console.log('you are logged in');
+    // link to home page
+    window.location.href = "./home.html";
+  }
+  else {
+    alert('wrong phone no or pin');
+  }
 })
